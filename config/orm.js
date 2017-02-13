29a62e8f7e8aecc554be678532a489c0df2f5c95
Brian Chang <Brian Chang>
2017-02-13 11:37:15 -0600

    updating handlebars template

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,38 @@
 var connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+var orm = {
+    all:function(tableInput, cb){
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, res){
+            if(err) throw err;
+
+            cb(res);
+        });
+    },
+    // INSERT INTO `burger_db`.`burgers` (`burger_name`) VALUES ('veggie');
+    create: function(tableInput, burgerName, cb){
+        var queryString = "INSERT INTO " + tableInput + "(burger_name) VALUES " + burgerName + ";";
+        connection.query(queryString, function(err, res){
+            if(err) throw err;
+            cb("New Burger added");
+        });
+    },
+    // UPDATE `burger_db`.`burgers` SET `is_eaten`='1' WHERE `burger_id`='4';
+    eaten: function(tableInput, burg, )
+
+};
+
+module.exports = orm;
--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,6 @@ USE burger_db;
 CREATE TABLE burgers(
 	burger_id INTEGER(11) auto_increment PRIMARY KEY,
     burger_name VARCHAR(100) NOT NULL,
-    is_eaten boolean NOT NULL
+    is_eaten boolean NOT NULL default 0
 
 );
