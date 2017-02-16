82d76884d94678cac0b4dfd706ff62e0fc41f9a7
Brian Chang <Brian Chang>
2017-02-16 11:27:05 -0600

    using clearDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+// Local connection
+/*
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "162605",
   database: "burger_db"
+});
+*/
+
+// CLEARDB CONNECTION
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "bcafda952ee0a5",
+  password: "b472594e",
+  database: "heroku_24bd092f1a8ecfe"
 });
 
 // Make connection.
--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,5 @@ CREATE TABLE burgers(
 	burger_id INTEGER(11) auto_increment PRIMARY KEY,
     burger_name VARCHAR(100) NOT NULL,
     is_eaten boolean NOT NULL default 0,
-	date datetime not null default now()
+	date timestamp not null default current_timestamp
 );
