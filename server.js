269de18bac842350ad23e0f972119604b27a59a8
Brian Chang <Brian Chang>
2017-02-16 12:02:23 -0600

    removed current URL

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var currentURL = window.location.origin;
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
