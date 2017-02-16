dd491dd253bf90b137c7db948e440b18f0091e2f
Brian Chang <Brian Chang>
2017-02-16 12:00:18 -0600

    npm errors on heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var path = require("path");
 
 var PORT = process.env.PORT || 3000;
 
